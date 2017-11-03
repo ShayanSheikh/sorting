655f26f6d7dab686d079981f6cc103a89e2948da
Shayan Sheikh <najam.shayan@gmail.com>
2017-11-03 09:41:05 -0400

    Done

--- mergesort.js
+++ mergesort.js
@@ -1,18 +1,22 @@
 function split(wholeArray) {
-  let mid = Math.floor(wholeArray.length/2);
+  let mid = wholeArray.length/2;
   return [wholeArray.slice(0, mid), wholeArray.slice(mid)];
 }
 
 function merge(arr1,arr2) {
-  var finalResult = [];
-  while(arr1.length > 0 && arr2.length > 0){
-    if(arr1[0] > arr2[0]) {
-      finalResult.push(arr2.shift());
+  var finalResult = [], leftInd = 0, rightInd = 0;
+  while(leftInd < arr1.length && rightInd < arr2.length){
+    if(arr1[leftInd] > arr2[rightInd]) {
+      finalResult.push(arr2[rightInd]);
+      rightInd++;
     } else { 
-      finalResult.push(arr1.shift());
+      finalResult.push(arr1[leftInd]);
+      leftInd++;
     }
   }
-  return finalResult.concat(arr1.concat(arr2));
+  finalResult = finalResult.concat(arr1.slice(leftInd));
+  finalResult = finalResult.concat(arr2.slice(rightInd));
+  return finalResult;
 }
 
 function mergeSort(arr) {
@@ -23,14 +27,3 @@ function mergeSort(arr) {
   //recursive part
   return merge( mergeSort(arr[0]), mergeSort(arr[1]) );
 }
-
-
-
-// function split(wholeArray){
-//   var final = [];
-//   //base case
-//   if(wholeArray.length<=1){
-//     return final.push([wholeArray]);
-//   }
-//   return final.push([wholeArray[0]]) + split(wholeArray.slice(1));
-// }
